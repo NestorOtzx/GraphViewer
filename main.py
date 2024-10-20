986f2be2d969a4ee92f6ccbb8ff2c0f1283b1d92
NestorOtzx <nestorortiz017@gmail.com>
2024-10-20 16:34:09 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from graphics import *
+from graphics import *
+
+
+def main():
+    win = GraphWin("My Circle", 500, 500)
+    c = Circle(Point(50,50), 10)
+    c.draw(win)
+    win.getMouse() # pause for click in window
+    win.close()
+main()
